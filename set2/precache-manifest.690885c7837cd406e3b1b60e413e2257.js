8c6c4ba060a627d7ec8cef7505c2840945c71338
Peter Podoski <hunpody@gmail.com>
2022-01-03 15:25:17 +0100

    add dialer version for set detector

---
+++ set2/precache-manifest.690885c7837cd406e3b1b60e413e2257.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/set2/favicon.ico"
+  },
+  {
+    "revision": "29bd67fc8c94a04db455dd6946ecfd39",
+    "url": "/set2/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/set2/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/set2/manifest.json"
+  },
+  {
+    "revision": "5487009520c0913b3a06",
+    "url": "/set2/static/css/main.40b796b8.chunk.css"
+  },
+  {
+    "revision": "5487009520c0913b3a06",
+    "url": "/set2/static/js/main.54870095.chunk.js"
+  },
+  {
+    "revision": "5a9a660a311124ef4678",
+    "url": "/set2/static/js/runtime~main.5a9a660a.js"
+  },
+  {
+    "revision": "354e092f0aa2734f8f4c",
+    "url": "/set2/static/js/vendors~main.354e092f.chunk.js"
+  }
+]);
